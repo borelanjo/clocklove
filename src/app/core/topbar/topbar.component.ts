5d18d40e48d69f1e4ebace663f94fcfd692c219c
Elton Leite <borelanjo@gmail.com>
2019-12-04 18:05:51 -0300

    Adiciona suporte a botão de adição, edição e exclusão na topbar

--- src/app/core/topbar/topbar.component.ts
+++ src/app/core/topbar/topbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
@@ -9,9 +9,27 @@ export class TopbarComponent implements OnInit {
 
   @Input() title = 'Clock Love';
   @Input() backLink: string;
+  @Input() showSave = false;
+  @Input() showEdit = false;
+  @Input() showDelete = false;
+  @Output() save: EventEmitter<any> = new EventEmitter();
+  @Output() edit: EventEmitter<any> = new EventEmitter();
+  @Output() delete: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {}
+
+  onSave() {
+    this.save.emit();
+  }
+
+  onEdit() {
+    this.edit.emit();
+  }
+
+  onDelete() {
+    this.delete.emit();
+  }
 
 }
