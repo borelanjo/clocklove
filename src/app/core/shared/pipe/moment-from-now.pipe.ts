e070ccc7634edf941d832c575f726c1ef8bfdf7a
Elton Leite <borelanjo@gmail.com>
2019-12-07 17:51:42 -0300

    Adiciona retorno de mensagem a Moment From Now Pipe

--- src/app/core/shared/pipe/moment-from-now.pipe.ts
+++ src/app/core/shared/pipe/moment-from-now.pipe.ts
@@ -6,11 +6,58 @@ import * as moment from 'moment';
 })
 export class MomentFromNowPipe implements PipeTransform {
 
+  // private toPt = {
+  //   year
+  // }
+
   transform(date: moment.MomentInput, converter: string) {
+    if (converter === 'message') {
+      return this.makeMessage(date);
+    }
     const now = moment();
     const before = moment(date);
     const duration = moment.duration(now.diff(before))[converter]();
+    if (Number(duration) === 0) {
+      return '';
+    }
     return duration;
+  }
+
+  private makeMessage(date: moment.MomentInput): string {
+    const fromNow = this;
+    const years = fromNow.transform(date, 'years');
+    const months = fromNow.transform(date, 'months');
+    const days = fromNow.transform(date, 'days');
+    const hours = fromNow.transform(date, 'hours');
+    const minutes = fromNow.transform(date, 'minutes');
+    const seconds = fromNow.transform(date, 'seconds');
+    let message = '';
+
+    if (years > 0) {
+      message += `${years} anos,`;
+    }
+
+    if (months) {
+      message += `${months} meses,`;
+    }
+
+    if (days) {
+      message += `${days} dias,`;
+    }
+
+    if (hours) {
+      message += `${hours} horas,`;
+    }
+
+    if (minutes) {
+      message += `${minutes} minutos,`;
+    }
+
+    if (seconds) {
+      message += `${seconds} segundos.`;
+    }
+
+    return message;
   }
 
 }
